d0bb773bbe482c92a67187cbd845a72d78e4e6cb
Fahri Maulana <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2022-03-11 14:37:44 +0700

    Add files via upload

---
+++ src/Angka.java
@@ -0,0 +1,13 @@
+public class Angka {
+    public int persegipanjang(int p, int l){
+        System.out.println("Luas persegi panjang");
+        System.out.println("Rumus: p x l");
+        return (p * l);
+    }
+
+    public double persegipanjang(double p, double l ){
+        System.out.println("Keliling persegi panjang");
+        System.out.println("Rumus: (2 x p) + (2 x l)");
+        return ((2 * p)+ (2 * l));
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Angka luas = new Angka();
+        Angka keliling = new Angka();
+
+        System.out.println(luas.persegipanjang(20,10));
+        System.out.println(keliling.persegipanjang(20.0, 10.0));
+    }
+}
